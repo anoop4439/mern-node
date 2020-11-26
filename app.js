5072f66cd70380d6d2da9ef6beb359dcad304a94
Anoop S Mohan <anoop4439@gmail.com>
2020-11-26 20:25:18 +0530

    users routes and functionality implemented

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placeRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 
 const app = express();
@@ -9,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/places', placeRoutes);
+
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Could not find the route.', 404);
